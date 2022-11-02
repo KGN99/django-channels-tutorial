a3599dbe5e23b847249c243d27416d20a012b90e
kgn99 <y3ef9484@gmail.com>
2022-11-02 16:51:10 +0900

    feat : Message 모델 추가 / 컨슈머 비동기식 해제

--- back/chat/models.py
+++ back/chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Message(models.Model):
+    author = models.ForeignKey(User,related_name='author_messages',on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    # 마지막 30번째 메시지까지 로드
+    def last_30_messages(self):
+        return Message.objects.order_by('-timestamp').all()[:30]
